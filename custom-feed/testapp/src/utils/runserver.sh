91928aba7befa6a3bd82cd97b6780bd4bd1007e9
Zech Rogers <zechr@skyrockettoys.com>
2018-11-19 09:51:43 -0800

    Add License to Recoil hub application Clean out test code

--- custom-feed/testapp/src/utils/runserver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-date +%c-%H:%M:%S.%N
-until ./zserver; do
-    echo "Server 'zserver' crashed with exit code $?.  Respawning.." >&2
-    sleep 5
-    echo "---------------------------------------------------------------------------------"
-    date +%c-%H:%M:%S.%N
-done
